62c8639b2f2e194fd4ff03ea736aadf90ca3a9b2
SegaraRai <SegaraRai@users.noreply.github.com>
2023-03-12 19:07:19 +0900

    chore: correct encoding and line endings

--- src/resource.h
+++ src/resource.h
@@ -1,13 +1,13 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ ‚Å¶¬‚³‚ê‚½ƒCƒ“ƒNƒ‹[ƒh ƒtƒ@ƒCƒ‹B
-// MuteApp.rc ‚Åg—p
+// Microsoft Visual C++ ã§ç”Ÿæˆã•ã‚ŒãŸã‚¤ãƒ³ã‚¯ãƒ«ãƒ¼ãƒ‰ ãƒ•ã‚¡ã‚¤ãƒ«ã€‚
+// MuteApp.rc ã§ä½¿ç”¨
 //
 #define IDI_ICON1                       101
 #define IDR_MENU1                       102
 #define ID_CONTEXTMENU_QUIT             40001
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
