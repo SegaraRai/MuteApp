ff4b17631c8fc37b3e402dedb1632272e0861a08
SegaraRai <SegaraRai@users.noreply.github.com>
2023-03-12 18:30:24 +0900

    build: migrate to CMake + Ninja + vcpkg, drop 32 bit (x86) support

--- Publish.bat
+++ Publish.bat
@@ -3,5 +3,4 @@
 RMDIR /S /Q Dist
 MKDIR Dist
 
-COPY Win32\Release\MuteApp.exe Dist\MuteApp-x86.exe
-COPY x64\Release\MuteApp.exe Dist\MuteApp-x64.exe
+COPY builds\Release\MuteApp.exe Dist\MuteApp.exe
